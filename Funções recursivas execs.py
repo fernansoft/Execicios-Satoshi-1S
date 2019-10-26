a5d032d3f7ea1dff734f5384eb9225b28077d824
fernansoft <54730133+fernansoft@users.noreply.github.com>
2019-10-26 14:05:17 -0300

    Update Funções recursivas execs.py

--- Funções recursivas execs.py
+++ Funções recursivas execs.py
@@ -2,43 +2,66 @@
 num = int(input('insira um número positivo desejado:'))
 def ffat(n):
   if (n==1):
-    return n  
+    return n
   else:
     f = (n * ffat(n-1))
   return f
 print ('valor fatorial', ffat(num))
 
-#Exercicio_01
+#serie01
 num = int(input('insira um número positivo:'))
 while num < 0:
   print('insira apenas valores positivos!')
   num = int(input('insira um número positivo'))
-def fsum(n):
+def fs01(n):
   if (n >= 100):
     return n
   else:
-    s = (n + fsum(n + 1))
+    s = (n + fs01(n + 1))
     return s
-print('A soma dos valores até 100 é:', fsum(num))
+print('A soma dos valores até 100 é:', fs01(num))
 
-#Exercicio_02
+#serie02
 num = int(input('insira o enésimo número: '))
-def fsumsub(n):
+def fs02(n):
+  if (n < 1):
+    return n
+  else:
+    s = (n + fs02(n - 1))
+    return s
+print('O resultado é:', fs02(num))
+
+#serie03
+num = float(input('digite o enésimo número: '))
+def fs03(n):
+  if (n <= 1):
+    return n
+  else:
+    s = ((n ** (-1)) + fs03(n-1))
+    return s
+print('o resultado é: ', fs03(num))
+
+#serie04
+def fs04(n, denominador):
+  if (n < 1):
+    return (n/denominador)
+  else:
+    return ((n/denominador) + fs04(n - 1, denominador + 1))
+    
+n1 = float(input('insira o numero: '))
+denominador = 1
+print(fs04(n1, denominador))
+
+#serie05
+def ffat(n):
+  if n == 1:
+    return n
+  else:
+    return (n * ffat(n - 1))
+def fless (n):
   if n < 1:
     return n
   else:
-    s = (n + fsumsub(n - 1))
-    return s
-print('O resultado é:', fsumsub(num))
-
-#Exercicio_03
-num = int(input('digite o enésimo número: '))
-def fsumfrac(n):
-  den = 1
-  if den >= n:
-    return n
-  else:
-    s = (1 / n + fsumfrac(1/(n + 1)))
-    den = den + 1
-    return s
-print('o resultado é: ', fsumfrac(num))
+    return (ffat(n) + fless(n - 1))
+num = int(input('Insira o enésimo termo da sequência: '))
+print ('O resultado é: ', fless(num))
