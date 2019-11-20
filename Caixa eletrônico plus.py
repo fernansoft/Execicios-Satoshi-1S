0b722254a02b8641625f8b2f2f882501f781fc58
fernansoft <fernansoft111@gmail.com>
2019-11-20 00:04:07 -0200

    dev parte 2 do menu

--- Caixa eletrônico plus.py
+++ Caixa eletrônico plus.py
@@ -23,3 +23,18 @@ def fcarregarnotas(a):
 	# a[5] = notas de R$05,00
 	# a[6] = notas de R$02,00
 	print('Pronto! Agora o caixa tem:\n{} notas de R$100,00\n{} notas de R$50,00\n{} notas de R$20,00\n{} notas de R$10,00\n{} notas de R$05,00\n{} notas de R$02,00'.format(a[0], a[1], a[2], a[3], a[4], a[5]))
+# 1.7. Coletar o código do banco que o cliente tem conta.
+vetbancos = ['Banco do Brasil', 'Santander', 'Itaú', 'Caixa']
+bancoselecionado = int(input('Qual o banco na qual deseja fazer o saque, digite de acordo com o código de cada um:\n1 - Banco do Brasil\n2 - Santander\n3 - Itaú\n4 - Caixa\n>>> '))
+	# Limitar input do usuário para apenas os códigos dos bancos
+while bancoselecionado != 1 and bancoselecionado != 2 and bancoselecionado != 3 and bancoselecionado != 4:
+	print('Banco não cadastrado! Favor digitar apenas um dos citados no menu de seleção.')
+	bancoselecionado = int(input('Qual o banco na qual deseja fazer o saque, digite de acordo com o código de cada um:\n1 - Banco do Brasil\n2 - Santander\n3 - Itaú\n4 - Caixa\n>>> '))
+
+# 1.2. Solicitar que o cliente faça a retirada de valores obedecendo ao critério do maior pelo menor. 
+# 1.3. Dar a opção para o cliente escolher o valor e a quantidade de notas. P. ex.: 1 x 20, 2 x 10 
+# 1.4. Caso não tenha o valor da maior cédula, disponibilizar a próxima. 
+# 1.5. Se o valor a ser solicitado for maior que o saldo total do caixa, enviar a mensagem “EXCEDEU O LIMITE DO CAIXA”. 
+# 1.6. Solicitar até 100 retiradas ou até não haver mais notas. 
+# 1.8. No final, exibir a estatística, separada por bancos, com: 
+# 1.8.1. O maior e o menor valor sacado; 1.8.2. A média dos saques; 1.8.3. Valor total dos saques; 1.8.4. Valor das sobras dos caixas.
